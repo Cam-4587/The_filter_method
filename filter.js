e7a53b5c07858ca82db6f80e9ab9d0b1d71a6559
Cam-4587 <162454229+Cam-4587@users.noreply.github.com>
2024-08-10 10:28:07 +0000

    The filter method

--- filter.js
+++ filter.js
@@ -20,7 +20,11 @@ const people = [
     age: 18,
   },
 ];
+const oldEnough = people.filter(person => person.age >= 21);
+console.log(oldEnough);
 
+const paul = people.filter(people => people.name === "Paul");
+console.log(paul)
 
 // Complex Filtering
 const students = [
@@ -55,3 +59,9 @@ const students = [
     ]
   },
 ];
+
+
+const has5yearsExp = skill => skill.yrsExperience >= 5;
+const hasStrongSkills = student => student.skills.filter(has5yearsExp).length > 0;
+const candidates = students.filter(hasStrongSkills);
+console.log(candidates);
